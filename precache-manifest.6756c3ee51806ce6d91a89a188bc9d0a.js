716d3905c440424ef92a428410ed68aef9880780
JOSHI-owo <ajithpkumar231@gmail.com>
2021-12-20 16:38:55 +0530

    Updates

---
+++ precache-manifest.6756c3ee51806ce6d91a89a188bc9d0a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e2bd42316d73ad53c96ad816261c3c",
+    "url": "/REACT/index.html"
+  },
+  {
+    "revision": "b3f949bb3548cea80332",
+    "url": "/REACT/static/css/2.02dddbd9.chunk.css"
+  },
+  {
+    "revision": "27f24eebfeaf26ceb92c",
+    "url": "/REACT/static/css/main.133f09d4.chunk.css"
+  },
+  {
+    "revision": "b3f949bb3548cea80332",
+    "url": "/REACT/static/js/2.285a8841.chunk.js"
+  },
+  {
+    "revision": "5b49d9c6117437bdc6f72f4b4b48c1bb",
+    "url": "/REACT/static/js/2.285a8841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa075fef510f774ac64",
+    "url": "/REACT/static/js/3.06725c94.chunk.js"
+  },
+  {
+    "revision": "27f24eebfeaf26ceb92c",
+    "url": "/REACT/static/js/main.bbaeb471.chunk.js"
+  },
+  {
+    "revision": "56b9f6026ea53b1843cf",
+    "url": "/REACT/static/js/runtime-main.b710fbec.js"
+  },
+  {
+    "revision": "ca9d0500d851ffdae77ecb682f1550c2",
+    "url": "/REACT/static/media/ajith.ca9d0500.jpg"
+  },
+  {
+    "revision": "4a6ca9de90e13d92eeaf77099ef05f28",
+    "url": "/REACT/static/media/bootstrap4.4a6ca9de.png"
+  },
+  {
+    "revision": "436cff5f03f38c1c0e472b3fb167a0b3",
+    "url": "/REACT/static/media/css3.436cff5f.png"
+  },
+  {
+    "revision": "0726a0c396d004c818db95c138a3192e",
+    "url": "/REACT/static/media/flask.0726a0c3.png"
+  },
+  {
+    "revision": "4f37a4f7de4e6599bfdf20b69a0a03a3",
+    "url": "/REACT/static/media/free-stock-image-1.4f37a4f7.jpg"
+  },
+  {
+    "revision": "a49f28819f63054beb64afca33d97fa4",
+    "url": "/REACT/static/media/free-stock-image-2.a49f2881.jpg"
+  },
+  {
+    "revision": "81cc280539d3f1b9dd4122ea1f3cf42e",
+    "url": "/REACT/static/media/free-stock-image-3.81cc2805.jpg"
+  },
+  {
+    "revision": "c3d882ebcc009714d36909ea88579b2d",
+    "url": "/REACT/static/media/html5.c3d882eb.png"
+  },
+  {
+    "revision": "82c50a10fb44dca388fdfa63cec49fb5",
+    "url": "/REACT/static/media/javascript.82c50a10.png"
+  },
+  {
+    "revision": "be2cace33f1da8314d1bfec4cc1660d4",
+    "url": "/REACT/static/media/mysql.be2cace3.png"
+  },
+  {
+    "revision": "aa172609998ea31780bc9090761e6448",
+    "url": "/REACT/static/media/python.aa172609.png"
+  },
+  {
+    "revision": "69d69642e98012f45f7cf312050b7dfa",
+    "url": "/REACT/static/media/react.69d69642.png"
+  }
+]);
