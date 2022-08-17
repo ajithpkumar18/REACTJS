6be0e57f6fff3c89a101f9c9c6eba2fc37d6f9cc
AJITH P. KUMAR <ajithpkumar231@gmail.com>
2022-08-17 13:26:53 +0530

    rcommendation from backend

--- src/components/RECCOMENDATION.js
+++ src/components/RECCOMENDATION.js
@@ -3,23 +3,22 @@ import { Consumer } from "../CONTEXT";
 import RECCOMENDATIONCARD from "./RECCOMENDATIONCARD";
 
 function RECCOMENDATION() {
-  return(
+  return (
     <Consumer>
-      {(value)=>{
-        const {details}=value
-return (
-  <div className="container-fluid my-5">
-    <div className="row text-center py-5 d-flex flex-nowrap overflow-auto scrollbar">
-      {details.map((detail) => (
-        <RECCOMENDATIONCARD uchiha={detail} />
-      ))}
-    </div>
-  </div>
-);
+      {(value) => {
+        const { details } = value;
+        return (
+          <div className="container-fluid my-5">
+            <div className="row text-center py-5 d-flex flex-nowrap overflow-auto scrollbar">
+              {details.map((detail) => (
+                <RECCOMENDATIONCARD key={detail.id} uchiha={detail} />
+              ))}
+            </div>
+          </div>
+        );
       }}
     </Consumer>
-  )
-  
+  );
 }
 
 export default RECCOMENDATION;
